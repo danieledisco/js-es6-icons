eb2f79f3ec04348c232a3f1518e3ccae6cdbf68f
danieledisco <daniele.disco.1965@gmail.com>
2023-12-16 14:08:33 +0100

    VERSIONE 2 Corretta la funzione che genera i numeri che arrivava fino a 10 invece che 15 (cifre Hex) Spezzato il codice in più funzioni Introdotti commenti

---
+++ js/functions.js
@@ -0,0 +1,113 @@
+/**
+ * createRandomColor
+ * 
+ * Questa funzione tramite generateRandomNumber(15) genera dei 
+ * numero casuali tra 0 e 15 e tramite la funzione toString(16) li
+ * trasforma in cifre esadecimali.
+ * Genera quindi una stringa di 7 caratteri che inizia con il carattere # e termina
+ * con le 6 cifre esadecimali
+ * 
+ * Autore DD - 12/2023 
+ * @return {string} 
+ */
+function createRandomColor()
+{
+	let color = '#';
+	for(let i=0; i<6; i++)
+	{
+		let char = generateRandomNumber(15).toString(16);
+		color += char;
+	}
+	return color;
+}
+
+/**
+ * generateRandomNumber
+ * 
+ * Questa funzione genera un numero intero casuale 
+ * compreso tra 0 e limit
+ * 
+ * Autore DD - 12/2023 (scopiazzata da w3s)
+ * @param {number} limit Rappresenta il massimo numero che può generare casualmente la funzione
+ * @return {number} il numero intero casuale generato tra 0 e limit
+ */
+function generateRandomNumber(limit)
+{
+	return Math.floor(Math.random() * limit);
+}
+
+/**
+ * createIconElement
+ * 
+ * Questa funzione accetta in ingresso il box di una pagina ed un elemento 
+ * di un array di oggetti e tramite il nome dell'oggetto costruice
+ * una icona appartenente alla Font Avesome, gli applica come stile il colore 
+ * e la aggancia al box passato in ingresso.
+ * Come ultimo passo aggiunge al box il nome dell'oggetto
+ * 
+ * @param {object} boxElement 
+ * @param {object} item 
+ * 
+ * Autore DD - 12/2023
+ */
+function createIconElement(boxElement, item) 
+{
+	const icons = document.createElement('i');
+	const listIcons = icons.classList;
+	listIcons.add("fa-solid");
+	listIcons.add("fa-" + item.name);
+//	icons.style.color = item.color;
+	icons.style.color = createRandomColor();
+	//icons.style.color = ("black");
+	boxElement.append(icons);
+	boxElement.innerHTML += item.name;
+}
+
+/**
+ * createBoxElement
+ * 
+ * Questa funzione accetta in ingresso un oggetto che contiene
+ * i parametri per costruire una icona.
+ * Dopo aver creato il box, tramite la funzione  createIconElement(boxElement, obj);
+ * si costruisce l'icona e si aggancia al box.
+ * Infine il box viene agganciato al conteiner che coniene box ed icone
+ * @param {object} obj
+ * 
+ * Autore DD - 12/2023 
+ */
+function createBoxElement(obj)
+{
+    const boxElement = document.createElement('div');
+    boxElement.className = 'box';
+    createIconElement(boxElement, obj);
+    iconsContainerEl.append(boxElement);
+}
+
+
+/**
+ * onChange
+ * 
+ * Funzione scatenata dall'evento cambia selezione nella
+ * select. 
+ * Prima di tutto si cancella il dom usando .innerHTML="".
+ * Poi si inizia a ciclare sugli elementi delle icone.
+ * Se il tipo coincide con la selezione si inserisce nella dom oppure
+ * se la selezione è all si inseriscono tutti
+ */
+function onChange() 
+{
+	iconsContainerEl.innerHTML="";
+	const text = e.options[e.selectedIndex].text;
+ 	for (let i = 0; i < listIcons.length; i++)
+	{
+		const obj = listIcons[i];
+		if (obj.type.localeCompare(text) == 0 || text === "all")
+		{
+            createBoxElement(obj);
+		}
+		else
+		{
+			continue;
+		}
+	}
+}
--- js/scripts.js
+++ js/scripts.js
@@ -24,239 +24,201 @@
  * 2 - popolare le options della select della milestone 3 dinamicamente.
  */
 const listIcons =
-	[
-		{
-			name: 'cat',
-			prefix: 'fa-',
-			type: 'animal',
-			family: 'fas',
-			color: 'orange'
-		}
-		,
-		{
-			name: 'crow',
-			prefix: 'fa-',
-			type: 'animal',
-			family: 'fas',
-			color: 'orange'
-		}
-		,
-		{
-			name: 'dog',
-			prefix: 'fa-',
-			type: 'animal',
-			family: 'fas',
-			color: 'orange'
-		}
-		,
-		{
-			name: 'dove',
-			prefix: 'fa-',
-			type: 'animal',
-			family: 'fas',
-			color: 'orange'
-		}
-		,
-		{
-			name: 'dragon',
-			prefix: 'fa-',
-			type: 'animal',
-			family: 'fas',
-			color: 'orange'
-		}
-		,
-		{
-			name: 'horse',
-			prefix: 'fa-',
-			type: 'animal',
-			family: 'fas',
-			color: 'orange'
-		}
-		,
-		{
-			name: 'hippo',
-			prefix: 'fa-',
-			type: 'animal',
-			family: 'fas',
-			color: 'orange'
-		}
-		,
-		{
-			name: 'fish',
-			prefix: 'fa-',
-			type: 'animal',
-			family: 'fas',
-			color: 'orange'
-		}
-		,
-		{
-			name: 'carrot',
-			prefix: 'fa-',
-			type: 'vegetable',
-			family: 'fas',
-			color: 'green'
-		}
-		,
-		{
-			name: 'apple-alt',
-			prefix: 'fa-',
-			type: 'vegetable',
-			family: 'fas',
-			color: 'green'
-		}
-		,
-		{
-			name: 'lemon',
-			prefix: 'fa-',
-			type: 'vegetable',
-			family: 'fas',
-			color: 'green'
-		}
-		,
-		{
-			name: 'pepper-hot',
-			prefix: 'fa-',
-			type: 'vegetable',
-			family: 'fas',
-			color: 'green'
-		}
-		,
-		{
-			name: 'user-astronaut',
-			prefix: 'fa-',
-			type: 'user',
-			family: 'fas',
-			color: 'blue'
-		}
-		,
-		{
-			name: 'user-graduate',
-			prefix: 'fa-',
-			type: 'user',
-			family: 'fas',
-			color: 'blue'
-		}
-		,
-		{
-			name: 'user-ninja',
-			prefix: 'fa-',
-			type: 'user',
-			family: 'fas',
-			color: 'blue'
-		}
-		,
-		{
-			name: 'user-secret',
-			prefix: 'fa-',
-			type: 'user',
-			family: 'fas',
-			color: 'blue'
-		}
-	];
-
-
-
-	const headerDownEl = document.getElementById("header_down");
-
-	const labelElement = document.createElement('div');
-	labelElement.className = "labelSelect";
-	labelElement.innerHTML += "FILTRA PER TIPO"
-	headerDownEl.append(labelElement);  
-	
-	
-	const arrayElementSelect = ['all','animal','vegetable','user'];
-	
-	const selectEl = document.createElement('select')
-	selectEl.className = 'selectType';
-
-	for(let i=0; i<arrayElementSelect.length; i++)
+[
 	{
-		const opt = document.createElement("option");
-		opt.value = i;
-		opt.innerHTML = arrayElementSelect[i];
-		selectEl.appendChild(opt);
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
 	}
+	,
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	}
+	,
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	}
+	,
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	}
+	,
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	}
+	,
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	}
+	,
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	}
+	,
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	}
+	,
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	}
+	,
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	}
+	,
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	}
+	,
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	}
+	,
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+	,
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+	,
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+	,
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+];
+/**
+ *  Lista delle variabili (Element) del problema
+ */
+
+
+
+
+
+
+/**
+ *  Inserimento label
+ */
+
+// 01 . Selezioniamo l'elemento del DOM a cui agganciare lable e select
+const headerDownEl = document.getElementById("header_down");
+// 02 . Generiamo l'etichetta 
+const labelElement = document.createElement('div');
+// 03 . Aggiungiamo la classe presente nel CSS
+labelElement.className = "labelSelect";
+// 04 . Inseriamo la lable
+labelElement.innerHTML += "FILTRA PER TIPO"
+// 05 . Agganciamo il div dell'etichetta all'elemento selezionato nel punto 01 
+headerDownEl.append(labelElement);  
+
+/**
+ *  Inserimento Select
+ */
+
+// 06 . Creiamo array con le voci della select
+const arrayElementSelect = ['all','animal','vegetable','user'];
+// 07 . Creiamo elemento select
+const selectEl = document.createElement('select');
+// 08 . Aggiungiamo alla select la classe 'selectType'
+selectEl.className = 'selectType';
+// 09 . Cicliamo sui vari elementi della select
+for(let i=0; i<arrayElementSelect.length; i++)
+{
+	// 09.1 . Creiamo option della select
+	const opt = document.createElement("option");
+	// 09.2 . Valorizziamo value con l'indice i del for
+	opt.value = i;
+	// 09.3 . Inseriamo l'elemento della select
+	opt.innerHTML = arrayElementSelect[i];
+	// 09.4 . Agganciamo la option alla select creata al punto 07
+	selectEl.appendChild(opt);
+}
 	
-	headerDownEl.append(selectEl);
+// 10 . Costruita la select nel punto 09, la agganciamo all'elemento selezionato nel punto 01
+headerDownEl.append(selectEl);
 
-
-
-
-
-	
-
+// 11 . Selezioniamo l'elemento che dovrà contenere tutte le cards
 const iconsContainerEl = document.querySelector(".iconsContainer");
 
+// 12 . Cicliamo sugli elementi per metterli in pagina tramite la 
+//		funzione createBoxElement(obj);
 for (let i = 0; i < listIcons.length; i++)
 {
-	const boxElement = document.createElement('div');
-	boxElement.className = 'box';
 	const obj = listIcons[i];
-	createIconAndBoxElement(boxElement, obj);
-	iconsContainerEl.append(boxElement);
+	createBoxElement(obj);
 }
 
+// 13 . Selezioniamo lelemento (evento) relativo alla select
+const e = document.querySelector('.selectType');
 
-
-
-
-function createIconAndBoxElement(boxElement, item) 
-{
-	const icons = document.createElement('i');
-	const listIcons = icons.classList;
-	listIcons.add("fa-solid");
-	listIcons.add("fa-" + item.name);
-//	icons.style.color = item.color;
-	icons.style.color = createRandomColor();
-	//icons.style.color = ("black");
-	boxElement.append(icons);
-	boxElement.innerHTML += item.name;
-}
-
-
-
-var e = document.querySelector('.selectType');
-function onChange() 
-{
-	console.log("Sono in onChange");
-	iconsContainerEl.innerHTML="";
-	var text = e.options[e.selectedIndex].text;
-	console.log(text);
- 	for (let i = 0; i < listIcons.length; i++)
-	{
-		const obj = listIcons[i];
-		if (obj.type.localeCompare(text) == 0 || text === "all")
-		{
-			const boxElement = document.createElement('div');
-			boxElement.className = 'box';
-			createIconAndBoxElement(boxElement, obj);
-			iconsContainerEl.append(boxElement);
-		}
-		else
-		{
-			continue;
-		}
-	}
-}
+// 14 . Aspettiamo qualora si cambi selezione alla select (evento)
 e.onchange = onChange;
-//onChange();
-
-function generateRandomNumber()
-{
-	return Math.floor(Math.random() * 10);
-}
-
-function createRandomColor()
-{
-	let color = '#';
-	for(let i=0; i<6; i++)
-	{
-		let char = generateRandomNumber().toString(16);
-		color += char;
-	}
-	return color;
-}
-
 
 
 
